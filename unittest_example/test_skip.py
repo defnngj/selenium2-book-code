adfed36a3b45cc9d69ac382420803a44c253c989
defnngj <defnngj@gmail.com>
2017-02-27 17:25:20 +0800

    update

--- unittest_example/test_skip.py
+++ unittest_example/test_skip.py
@@ -1,3 +1,6 @@
+'''
+跳过测试用例。
+'''
 import unittest
 
 
@@ -8,19 +11,19 @@ class MyTest(unittest.TestCase):
 
     def tearDown(self):
         pass
-    '''
-    @unittest.skip("直接跳过测试")  #裝飾器
+
+    @unittest.skip("直接跳过测试")
     def test_skip(self):
         print("test aaa")
-    
+
     @unittest.skipIf(3 > 5, "当条件为True时跳过测试")
     def test_skip_if(self):
         print('test bbb')
-    
+
     @unittest.skipUnless(3 > 5, "当条件为True时执行测试")
     def test_skip_unless(self):
         print('test ccc')
-    '''
+
     @unittest.expectedFailure
     def test_expected_failure(self):
         self.assertEqual(2, 3)
