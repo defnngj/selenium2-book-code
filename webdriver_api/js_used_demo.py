0735ebb9bfbf5de36d0dc66f8a238e5e129f581e
fnngj <fnngj@126.com>
2018-08-03 17:05:04 +0800

    add JS used demo

---
+++ webdriver_api/js_used_demo.py
@@ -0,0 +1,13 @@
+"""
+Author: 虫师
+Date: 2018/8/3
+desc: 记录js 的一些用法
+"""
+
+# 向富文本框输入内容
+js = 'document.querySelector(".nui-scroll").innerHTML="this is text";'
+
+
+# 使隐藏的属性显示出来
+js2 = 'document.querySelectorAll("select")[0].style.display="block";'
+
